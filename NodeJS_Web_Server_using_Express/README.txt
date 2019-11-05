8d48c9cbc3b68303b0a6c05615e2f1c54f207272
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-11-05 19:50:25 +0700

    Using Pug template engines with Express

---
+++ NodeJS_Web_Server_using_Express/README.txt
@@ -0,0 +1,5 @@
+Using Pug template engines with Express
+
+References: 
+            + https://expressjs.com/en/guide/using-template-engines.html
+            + https://pugjs.org/api/getting-started.html
--- NodeJS_Web_Server_using_Express/index.js
+++ NodeJS_Web_Server_using_Express/index.js
@@ -1,8 +1,20 @@
 const Express = require('express')
 const app = Express()
 const port = 3000
-
+app.set('views', './views')
+app.set('view engine', 'pug')
 app.get('/',(req, res) => {
-    res.send('Hello World!');
+    res.render('index',{
+        name : "Hello Word"
+    })
+})
+app.get('/users',(req, res) =>{
+    res.render('./users/index',{
+        arrUser : [{
+            id: 1,name:"Tom"
+        },{
+            id:2,name:"Jerry"
+        }]
+    })
 })
 app.listen(port, () => console.log(`Server listening on port ${port}`))
