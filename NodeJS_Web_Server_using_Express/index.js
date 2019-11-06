f777e9e55671c5dd0382fe4349992764e903d7f5
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-11-06 11:21:54 +0700

    Lesson3: Query parameters

--- NodeJS_Web_Server_using_Express/index.js
+++ NodeJS_Web_Server_using_Express/index.js
@@ -3,6 +3,11 @@ const app = Express()
 const port = 3000
 app.set('views', './views')
 app.set('view engine', 'ejs')
+
+
+listUser =[{id: 1,name:"Tom"},{id:2,name:"Jerry"}]
+
+
 app.get('/',(req, res) => {
     res.render('index',{
         name : "Hello Word"
@@ -10,11 +15,18 @@ app.get('/',(req, res) => {
 })
 app.get('/users',(req, res) =>{
     res.render('./users/index',{
-        arrUser : [{
-            id: 1,name:"Tom"
-        },{
-            id:2,name:"Jerry"
-        }]
+        arrUser :listUser
     })
 })
+
+app.get('/users/search', (req, res) =>{
+    var q = req.query.q
+    var result = listUser.filter((user) =>{
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !==-1
+    })
+    res.render('./users/index',{
+        arrUser: result
+    })
+})
+
 app.listen(port, () => console.log(`Server listening on port ${port}`))
