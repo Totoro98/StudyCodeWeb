fb2d96640593c780103ab418a52f0631ec7cf895
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-11-10 02:02:11 +0700

    Lesson 20: Pagination (P.2) - Create Pagination

--- NodeJS_Web_Server_using_Express/controllers/product.controller.js
+++ NodeJS_Web_Server_using_Express/controllers/product.controller.js
@@ -1,7 +1,25 @@
 const db = require('../db')
 module.exports.product = (req, res)=>{
-    var products = db.get('products').value()
+    var page = parseInt(req.query.page) || 1
+    var temp
+    if(page==1){
+        arrPage = [page,page+1,page+2]
+        temp = true
+    }else{
+        arrPage = [page-1,page,page+1]
+        temp=false
+    }
+    var perPage = 8
+    var start = (page - 1)*perPage
+    var end = page*perPage
+    var products =  db.get('products').value().slice(start, end)
     res.render('./products/index',{
-        products: products, user : undefined
+        products: products, 
+        user : undefined,
+        data: {
+            pageSelected: page,
+            arrPage: arrPage,
+            temp: temp
+        }
     })
 }
