dda40772773d5d0575ae028984cc81c37b7ee83f
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-11-13 00:42:44 +0700

    Lesson 8: Dynamic class names(Part 1)

--- ReactJS/my-app/src/App.js
+++ ReactJS/my-app/src/App.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
+import './components/TodoItem.css';
 
 class App extends Component{
   constructor(){
     super();
     this.array= [
-      'Mining data',
-      'Xamarin',
-      'Olap'
+      {title: 'Mining data',iscomplete: true},
+      {title: 'Xamarin',iscomplete: false},
+      {title: 'Olap', iscomplete: false}
     ]
   }
   render(){
     return (
       <div className="App">
         {
-          this.array.map((item, index)=><TodoItem key={index} title={item} />)
+          this.array.map((item, index)=>
+            <TodoItem key={index} item={item} />)
         }
       </div>
     );
--- ReactJS/my-app/src/components/TodoItem.js
+++ ReactJS/my-app/src/components/TodoItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 
 class TodoItem extends Component {
     render(){
+        // const item = this.props.item; cách 1
+        const {item} = this.props; //cách 2, destructuring assignment syntax 
+        //{item} = { item : {title:"Olap",iscomplete:true}}
+        //console.log(item) => {title:"Olap",iscomplete:true}
+        let className = "TodoItem"
+        if(item.iscomplete){
+            className +=" TodoItem-complete"
+        }
         return (
-            <div className="TodoItem">
-                <p>{this.props.title}</p>
+            <div className={className}>
+                <p>{item.title}</p>
             </div>
         );
     }
