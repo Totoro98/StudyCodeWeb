5269a9b9c5a536e014e53228ac4982b971f80eec
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-11-14 23:35:38 +0700

    Lesson 9: Conditional rendering

--- ReactJS/my-app/src/App.js
+++ ReactJS/my-app/src/App.js
@@ -8,18 +8,20 @@ class App extends Component{
   constructor(){
     super();
     this.array= [
-      {title: 'Mining data',iscomplete: true},
-      {title: 'Xamarin',iscomplete: false},
-      {title: 'Olap', iscomplete: false}
+      // {title: 'Mining data',iscomplete: true},
+      // {title: 'Xamarin',iscomplete: false},
+      // {title: 'Olap', iscomplete: false}
     ]
   }
   render(){
     return (
       <div className="App">
         {
+          this.array.length > 0 &&
           this.array.map((item, index)=>
             <TodoItem key={index} item={item} />)
         }
+        {this.array.length == 0 && 'Notthing here'}
       </div>
     );
   }
