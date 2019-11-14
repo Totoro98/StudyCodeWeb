35bc4cd8f18ae3c231beda39c879912559ccc541
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-11-14 23:23:05 +0700

    Lesson 8: Dynamic class names(Part 2) using classNames

--- ReactJS/my-app/src/components/TodoItem.js
+++ ReactJS/my-app/src/components/TodoItem.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-
+var classNames = require('classnames');
 class TodoItem extends Component {
     render(){
         // const item = this.props.item; cách 1
         const {item} = this.props; //cách 2, destructuring assignment syntax 
         //{item} = { item : {title:"Olap",iscomplete:true}}
         //console.log(item) => {title:"Olap",iscomplete:true}
-        let className = "TodoItem"
+        
+        var classname = classNames('TodoItem')
+        
         if(item.iscomplete){
-            className +=" TodoItem-complete"
+            classname = classNames('TodoItem','TodoItem-complete')
         }
         return (
-            <div className={className}>
+            <div className={classname}>
                 <p>{item.title}</p>
             </div>
         );
