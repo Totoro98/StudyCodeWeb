dff254297907247d0e5a26df3f809e6c35d0b364
tuanduongnguyen98 <ntdmonter@gmail.com>
2019-11-20 23:11:24 +0700

    Lesson 11: Handling events with data attibute, event target

--- ReactJS/my-app/src/App.js
+++ ReactJS/my-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
 import './components/TodoItem.css';
@@ -7,21 +6,37 @@ import './components/TodoItem.css';
 class App extends Component{
   constructor(){
     super();
-    this.array= [
-      // {title: 'Mining data',iscomplete: true},
-      // {title: 'Xamarin',iscomplete: false},
-      // {title: 'Olap', iscomplete: false}
-    ]
+    this.index = null
+    this.state = {
+      array : [
+        {title: 'Mining data',iscomplete: true},
+        {title: 'Xamarin',iscomplete: false},
+        {title: 'Olap', iscomplete: false}
+      ]
+    }
+    
+    this.onItemClick= this.onItemClick.bind(this)
+  }
+  onItemClick(event){
+    var p = event.target
+    var i= parseInt(p.dataset.id)
+
+    var array = this.state.array
+    array[i].iscomplete = !array[i].iscomplete
+     
+    this.setState(state => ({
+      array : array
+    }));
   }
   render(){
     return (
       <div className="App">
         {
-          this.array.length > 0 &&
-          this.array.map((item, index)=>
-            <TodoItem key={index} item={item} />)
+          this.state.array.length > 0 &&
+          this.state.array.map((item, index)=>
+            <TodoItem  item={item} id={index} onItemClick = {this.onItemClick} />)
         }
-        {this.array.length == 0 && 'Notthing here'}
+        {this.state.array.length === 0 && 'Notthing here'}
       </div>
     );
   }
--- ReactJS/my-app/src/components/TodoItem.js
+++ ReactJS/my-app/src/components/TodoItem.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 var classNames = require('classnames');
 class TodoItem extends Component {
+    constructor(props){
+        super(props)
+
+        // this.onItemClick = this.onItemClick.bind(this)
+    }
+    // onItemClick(){
+        
+    //    console.log(this.props.item);
+        
+    // }
     render(){
         // const item = this.props.item; cách 1
-        const {item} = this.props; //cách 2, destructuring assignment syntax 
+        const {item, onItemClick, id} = this.props; //cách 2, destructuring assignment syntax 
         //{item} = { item : {title:"Olap",iscomplete:true}}
         //console.log(item) => {title:"Olap",iscomplete:true}
         
         var classname = classNames('TodoItem')
-        
         if(item.iscomplete){
             classname = classNames('TodoItem','TodoItem-complete')
         }
         return (
-            <div className={classname}>
-                <p>{item.title}</p>
+            <div onClick={onItemClick} className={classname}>
+                <p id={id} data-id={id} >{item.title}</p>
             </div>
         );
     }
